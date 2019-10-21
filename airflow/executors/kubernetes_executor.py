dc781ca02f9ff3256dc675e0be4861cf6debdaa6
wideet <wideet@gmail.com>
2019-10-21 10:21:06 -0700

    fix: read bytes to open GCP credentials JSON w/ K8s Executor Also, update default cfg w/ correct comments

--- airflow/executors/kubernetes_executor.py
+++ airflow/executors/kubernetes_executor.py
@@ -673,7 +673,7 @@ class KubernetesExecutor(BaseExecutor, LoggingMixin):
                 return self.kube_client.create_namespaced_secret(
                     self.kube_config.executor_namespace, kubernetes.client.V1Secret(
                         data={
-                            'key.json': base64.b64encode(open(secret_path, 'r').read())},
+                            'key.json': base64.b64encode(open(secret_path, 'rb').read())},
                         metadata=kubernetes.client.V1ObjectMeta(name=secret_name)),
                     **self.kube_config.kube_client_request_args)
             except ApiException as e:
@@ -682,7 +682,7 @@ class KubernetesExecutor(BaseExecutor, LoggingMixin):
                         secret_name, self.kube_config.executor_namespace,
                         kubernetes.client.V1Secret(
                             data={'key.json': base64.b64encode(
-                                open(secret_path, 'r').read())},
+                                open(secret_path, 'rb').read())},
                             metadata=kubernetes.client.V1ObjectMeta(name=secret_name)),
                         **self.kube_config.kube_client_request_args)
                 self.log.exception(
